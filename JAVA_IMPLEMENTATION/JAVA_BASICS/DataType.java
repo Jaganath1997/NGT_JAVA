b8e03f6ff7fe940bf7530e940786ee10a6bca716
Jaganath1997 <105206943+Jaganath1997@users.noreply.github.com>
2022-05-09 16:23:43 +0530

    Add files via upload

---
+++ JAVA_IMPLEMENTATION/JAVA_BASICS/DataType.java
@@ -0,0 +1,27 @@
+package basics;
+
+public class DataType {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//primitive datatype
+        int a = 10;
+        //int- datatype; a- variable; 10- value;
+        float b = 5.5f;
+        char c ='j';
+        boolean jagan =true;
+        short d = 200;
+        double e = 567.4d;
+        long f = 500000L;
+        byte g = 10;// \u0000 is an ascii code system
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(jagan);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(g);
+	}
+
+}
