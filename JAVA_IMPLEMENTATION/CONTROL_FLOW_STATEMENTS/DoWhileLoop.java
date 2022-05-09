b10ce8390751ad24ad8dc12b49685ceab7138cd4
Jaganath1997 <105206943+Jaganath1997@users.noreply.github.com>
2022-05-09 16:16:00 +0530

    Add files via upload

---
+++ JAVA_IMPLEMENTATION/CONTROL_FLOW_STATEMENTS/DoWhileLoop.java
@@ -0,0 +1,19 @@
+package basics;
+
+public class DoWhileLoop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a1= 1;//initialization
+        do {
+        System.out.println(a1+") jagan");
+        a1++;
+        }while(a1<6);
+        int a2= 6;//initialization
+        do {
+        System.out.println("jack");
+        a2--;
+        }while(a2>1);
+	}
+
+}
---
+++ JAVA_IMPLEMENTATION/CONTROL_FLOW_STATEMENTS/WhileLoop.java
@@ -0,0 +1,21 @@
+package basics;
+
+public class WhileLoop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        int i = 1;//initialization
+        while(i<7)//condition 
+        	{
+        	System.out.println("hi! this is jaganath");
+        	i++;//increment operator
+        }
+        int j=5;
+        while(j>1)
+        {
+        	System.out.println("how are you?");
+        	j--;
+        }
+	}
+
+}
