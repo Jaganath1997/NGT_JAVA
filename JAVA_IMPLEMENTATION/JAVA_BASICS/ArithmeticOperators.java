df4ff6192056a93cb0d4fecafa1ef9ad06baebe2
Jaganath1997 <105206943+Jaganath1997@users.noreply.github.com>
2022-05-09 16:44:27 +0530

    Add files via upload

---
+++ JAVA_IMPLEMENTATION/JAVA_BASICS/ArithmeticOperators.java
@@ -0,0 +1,27 @@
+package basics;
+
+public class ArithmeticOperators {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=6, b=4;
+		int o1=a+b;
+		int o2=a-b;
+		int o3=a*b;
+		//int o4=a/b; it wont give decimal value
+		double o4=(double)a/b; //cast operator(explicit conversion)
+		int o5=a%b;//reminder
+		
+		
+		System.out.println(o1);
+		System.out.println(o2);
+		System.out.println(o3);
+		System.out.println(o4);
+		System.out.println(o5);
+		
+		
+
+
+	}
+
+}
