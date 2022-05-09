1687304d572a598d3f7ed2b435f97978060b74d9
Jaganath1997 <105206943+Jaganath1997@users.noreply.github.com>
2022-05-09 16:18:38 +0530

    Add files via upload

---
+++ JAVA_IMPLEMENTATION/JAVA_BASICS/AssignmentOperator.java
@@ -0,0 +1,21 @@
+package basics;
+
+public class AssignmentOperator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 6,b = 5;
+		a+=b;
+		System.out.println("add "+a);
+		a-=b;
+	    System.out.println("sub "+a);
+	    a*=b;
+	    System.out.println("mul "+a);
+	    a/=b;
+	    System.out.println("div "+a);
+	    a%=b;
+	    System.out.println("mod "+a);
+
+	}
+
+}
---
+++ JAVA_IMPLEMENTATION/JAVA_BASICS/Inc_Dec_operator.java
@@ -0,0 +1,22 @@
+package basics;
+
+public class Inc_Dec_operator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub 
+		int count=10;
+		
+		//post increment
+		System.out.println(count++);
+		//pre increment
+		System.out.println(++count);
+		
+		int count1=15;
+		
+		//post decrement
+		System.out.println(count1--);
+		//pre decrement
+		System.out.println(--count1);
+	}
+
+}
