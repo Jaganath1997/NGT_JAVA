a65c5797b6ad9a912665f66a9c4b1e501254cf3a
JAGANATH G <105206943+JaganathGopalOff@users.noreply.github.com>
2022-05-11 11:53:28 +0530

    Add files via upload

---
+++ JAVA_ASSIGNMENT/mapToList.java
@@ -0,0 +1,24 @@
+package Assignment;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class mapToList {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		HashMap<String, Integer> hashMap = new HashMap<>();
+		hashMap.put("a", 1);
+		hashMap.put("b", 2);
+		hashMap.put("c", 3);
+		hashMap.put("d", 4);
+		hashMap.put("e", 5);
+		List <String> key =new ArrayList<>(hashMap.keySet());
+		System.out.println(key);
+		List <Integer> value =new ArrayList<>(hashMap.values());
+		System.out.println(value);
+		}
+	}
+
+
