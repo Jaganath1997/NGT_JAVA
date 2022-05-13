86d5b0f4359f2a0b6acf97bf1b1aba299d166083
JAGANATH G <105206943+JaganathGopalOff@users.noreply.github.com>
2022-05-13 10:23:31 +0530

    Add files via upload

---
+++ JAVA_ASSIGNMENT/PrintAlphabet.java
@@ -0,0 +1,13 @@
+package Assignment;
+
+public class PrintAlphabet {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println("alphabet are: ");
+        for(char ch='a';ch<='z';ch++) {
+        	System.out.print(ch+", ");
+        }
+	}
+
+}
